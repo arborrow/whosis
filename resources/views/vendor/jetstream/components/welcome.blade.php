e9618c8517b46866536547472f0be72a7453d65a
Anthony Borrow <arborrow@jesuits.net>
2021-04-09 22:34:48 -0600

    Updating welcome blade

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -4,14 +4,13 @@
     </div>
 
     <div class="mt-8 text-2xl">
-        Welcome to your Jetstream application!
+        Welcome to WhoSIS!
     </div>
 
     <div class="mt-6 text-gray-500">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
+        WhoSIS is a Student Information System loosely based upon OpenSIS built with Laravel Jetstream, Livewire and Tailwind CSS.
+        We believe you should be able to focus on your students rather than the technology to keep track of them.
+        We hope you love it.
     </div>
 </div>
 
